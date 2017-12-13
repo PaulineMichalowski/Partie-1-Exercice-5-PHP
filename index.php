b62fb104fd276ee41a04b4b0b6f51776c465d035
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 09:10:07 +0100

    Correction ex5, ajout du settype pour donner un type à la variable

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<?php
-$variable_vide = null;
-echo $variable_vide;
-$variable_vide = 12;
-echo $variable_vide;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 5</title>
+</head>
+<body>
+  <?php
+  $variable_vide = null;
+  settype($variable_vide, 'integer');
+  echo $variable_vide;
+  $variable_vide = 12;
+  echo $variable_vide;
+  ?>
+</body>
+</html>
